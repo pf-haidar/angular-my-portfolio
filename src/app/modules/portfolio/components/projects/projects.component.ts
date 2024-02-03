f1530d43448f97ace612c9f8a80ad4a4915b1e2c
pf-haidar <pedrohaidarfilho@gmail.com>
2024-02-02 20:32:52 -0400

    feat dialog component

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,16 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 
 // Interface
 import { IProjects } from '../../interface/IProjects.interface';
+
+// Material
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+// Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+// Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   //TODO adicionar meus próprios projetos
   public arrayProjects = signal<IProjects[]>([
     {
@@ -19,13 +30,20 @@ export class ProjectsComponent {
       title: 'Vida Fullstack',
       width: '100px',
       height: '51px',
-      description: '<p>Acesse já!!!</p>',
+      description: '<p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa</p>',
       links: [
         {
           name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br'
-        }
-      ]
-    }
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    });
+  }
 }
